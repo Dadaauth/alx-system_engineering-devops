bba39eec6417c66d9e9e220b8c8418de008abc5a
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-12-15 12:47:13 -0800

    changed the version of flask to install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # installs flask from pip3
 $package_name = 'flask'
 exec {"install ${package_name}":
-  command => "/usr/bin/pip3 install ${package_name}",
+  command => "/usr/bin/pip3 install ${package_name}==2.1.0",
   unless  => "/usr/bin/pip3 show ${package_name}"
 }
