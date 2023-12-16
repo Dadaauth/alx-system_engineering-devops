63f8d0c09a95657d31e2de2df1ca4475e6a048f3
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-12-16 13:57:11 +0100

    Updated 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # installs flask from pip3
 $package_name = 'flask'
+
+package {'python3-pip':
+  ensure => 'installed'
+}
+
+
 exec {"install ${package_name}":
-  command => "/usr/bin/pip3 install ${package_name}==2.1.0",
-  unless  => "/usr/bin/pip3 show ${package_name}"
+  command => "/usr/bin/pip3 install ${package_name}==2.1.0 werkzeug==2.1.1",
+  require => Package['python3-pip'],
 }
