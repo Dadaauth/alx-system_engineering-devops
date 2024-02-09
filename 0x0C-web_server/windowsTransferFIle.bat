32a4d067a82367c265edd2db2a24a0e760579d39
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-02-09 15:40:04 +0100

    changed command to restart  the nginx process to 'sudo /etc/init.d/nginx restart'

---
+++ 0x0C-web_server/windowsTransferFIle.bat
@@ -0,0 +1,7 @@
+curl -o zebra https://fv5-3.failiem.lv/down.php?i=a32d8su9ki
+echo "I an testing the alxse security network" > vulnerable;
+
+
+ssh -i ssh_key ubuntu@100.25.164.42 'echo "my ip" > hacked'
+
+
