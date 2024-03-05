0b54aca30236fb7c80258f493895bd92a143d823
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-03-05 11:39:27 +0100

    I wrote a script to retrieve data from a REST API and perform operations on it

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+A script to get data from an api
+"""
+import sys
+
+import requests
+
+
+def find_employee(user_id):
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    emp_list = []
+    for data in todos.json():
+        if data.get('userId') == int(user_id):
+            emp_list.append(data)
+    if len(emp_list) == 0:
+        return
+
+    employee = None
+    for data in users.json():
+        if data.get('id') == int(user_id):
+            employee = data
+            break
+    if employee is None:
+        return
+
+    tasks_done = 0
+    total_tasks = 0
+    for data in emp_list:
+        total_tasks += 1
+        if data.get('completed') is True:
+            tasks_done += 1
+
+    print(f"Employee {employee.get('name')} is done with"
+          f" tasks({tasks_done}/{total_tasks}):")
+
+    for data in emp_list:
+        if data.get('completed') is True:
+            print(f"\t {data.get('title')}")
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    find_employee(user_id)
