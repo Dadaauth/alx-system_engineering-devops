54b19c082e85eb9605f2dd7bc6af0814e2518533
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-03-12 18:26:28 +0100

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Define 
+# Define
 exec { 'increase nginx file open limit':
-  command => '/bin/sed -i "s/-n 15/-n 15000/" /etc/default/nginx',
+  command => '/bin/sed -i "s/-n 15/-n 15000/" /etc/default/nginx && service nginx restart',
 }
